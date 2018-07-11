44fc9c953d5f014f33202f39d0f56042ed38461e
uhyo <uhyo@uhy.ooo>
2018-07-11 18:48:26 +0900

    give JVM more initial memory

--- bench.sh
+++ bench.sh
@@ -8,7 +8,7 @@ echo "Running the Rust implementation..."
 rust/target/release/rustbf $INPUT $REPEAT >> results/rust.txt
 
 echo "Running the Scala implementation..."
-scala/target/universal/stage/bin/bf $INPUT $REPEAT >> results/scala.txt
+scala/target/universal/stage/bin/bf -J-Xmx2048m -J-Xms1024m $INPUT $REPEAT >> results/scala.txt
 
 echo "Running the JavaScript implementation..."
 node js/index.js $INPUT $REPEAT >> results/js.txt
